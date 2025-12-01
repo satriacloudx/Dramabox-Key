7c0b7fc2ff45edc5390e6b475ef4bee0b4f4f59a
satriacloudx <133637951+satriacloudx@users.noreply.github.com>
2025-12-01 22:45:07 +0700

    Update search.js

--- api/search.js
+++ api/search.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async function handler(req, res) {
     try {
         // Get token
-        const tokenResponse = await axios.get("https://dramabox-token.vercel.app/token");
+        const tokenResponse = await axios.get("https://dramabox-key.vercel.app/token");
         const gettoken = tokenResponse.data;
 
         const url = "https://sapi.dramaboxdb.com/drama-box/search/suggest";
@@ -49,4 +49,5 @@ export default async function handler(req, res) {
             error: error.message
         });
     }
+
 }
